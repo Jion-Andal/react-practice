d30cf8ad846e37c1780144799954f243acb5e4d1
jion-andal <andaljion@gmail.com>
2024-06-02 22:23:03 +0800

    Using of useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,27 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 function ListGroup() {
   let items = ["Namunga", "Namuco", "Masaya", "Bagong Pook", "Quilib"];
+  // let selectedIndex = -1;
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const headingText = <h1>List</h1>;
 
   const noItems = <p>No items found</p>;
 
   const handleClick = (event: MouseEvent) => console.log(event);
+  const handleClickName = (index: number) => setSelectedIndex(index);
 
   const mapItems = () => {
     return items.map((item, index) => (
-      <li className="list-group-item" key={item} onClick={handleClick}>
+      <li
+        className={
+          selectedIndex === index ? "list-group-item active" : "list-group-item"
+        }
+        key={item}
+        onClick={() => handleClickName(index)}
+      >
         {item}
       </li>
     ));
